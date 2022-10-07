63ca03fff3f081ba9e87b5fe68358ceea517b882
apps.prak <apps.prak@gmail.com>
2022-10-08 02:57:29 +0530

    2nd commit in master/main branch

--- GradaleDummyProject/src/main/java/com/java/eclipsegit/Test3.java
+++ GradaleDummyProject/src/main/java/com/java/eclipsegit/Test3.java
@@ -3,6 +3,7 @@ package com.java.eclipsegit;
 public class Test3 {
     int a;
     int b;
+    int c;
 
     public static void main(String [] args){
         System.out.println("This is test3 file");
