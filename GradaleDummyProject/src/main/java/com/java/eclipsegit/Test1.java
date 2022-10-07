edab4b2356535af6e20071384a1228a4bfd96503
Prakash <apps.prak@gmail.com>
2022-10-08 02:43:21 +0530

    Initial commit in master/main branch

---
+++ GradaleDummyProject/src/main/java/com/java/eclipsegit/Test1.java
@@ -0,0 +1,10 @@
+package com.java.eclipsegit;
+
+public class Test1 {
+    int a;
+    int b;
+
+    public static void main(String [] args){
+        System.out.println("This is test1 file");
+    }
+}
---
+++ GradaleDummyProject/src/main/java/com/java/eclipsegit/Test2.java
@@ -0,0 +1,10 @@
+package com.java.eclipsegit;
+
+public class Test2 {
+    int a;
+    int b;
+
+    public static void main(String [] args){
+        System.out.println("This is test2 file");
+    }
+}
